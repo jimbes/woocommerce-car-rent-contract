d797488b725dee039479b6040f18d9379877f390
Jim <besse.jimmy@gmail.com>
2021-07-28 23:18:20 +0200

    Forget to change version in descriptio file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # woocommerce_car_rent_contract
+
+<ul>
+<li>
+1.6 Add Docage </li>
+<li>
+1.5 Add Image and multiple page contract
+</li>
+<li>
+1.4 Create PDF without pdf form
+</li>
--- woocommerce-car-rent-contract.php
+++ woocommerce-car-rent-contract.php
@@ -3,7 +3,7 @@
  * Plugin Name: Woocommerce Car rent Contract
  * Plugin URI: http://www.jimmy-besse.fr/woocommerce-car-rent-contract
  * Description: Ability to create contract from Woocommerce Order.
- * Version: 1.5
+ * Version: 1.6
  * Author: Jimmy Besse
  * Author URI: http://www.jimmy-besse.fr
  */
